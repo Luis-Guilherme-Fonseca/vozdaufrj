ebea4474ed37e472f0197d836df4e0675536ed3b
luis quirino <luis quirino>
2016-10-07 05:36:33 -0300

    troquei a rota home

--- vufrj/vozdaufrj/resources/views/templates/layoutAdmin.blade.php
+++ vufrj/vozdaufrj/resources/views/templates/layoutAdmin.blade.php
@@ -99,7 +99,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{route('home')}}">HOME</a>
+            <a class="navbar-brand" href="{{route('principal')}}">HOME</a>
         </div>
 
         <div class="collapse navbar-collapse" id="navbar-collapse-1">
